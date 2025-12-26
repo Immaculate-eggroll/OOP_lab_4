e09319880c70cb2255074d1c917d744d4b8b92d5
Immaculate-eggroll <blessed.potato@yahoo.com>
2025-12-26 15:33:11 +0300

    Update tests.cpp

--- tests.cpp
+++ tests.cpp
@@ -1,64 +1,73 @@
-#include <gtest/gtest.h>
-#include "array.h"
+#include "point.h"
 #include "trapezoid.h"
 #include "rhombus.h"
 #include "pentagon.h"
+#include "array.h"
+#include <gtest/gtest.h>
+#include <memory>
+#include <cmath>
 
-TEST(PointTest, DefaultConstructor) {
-    Point<int> p;
-    EXPECT_EQ(p.getX(), 0);
-    EXPECT_EQ(p.getY(), 0);
+TEST(PointTest, CreationAndAccess) {
+    Point<int> p(3, 4);
+    EXPECT_EQ(p.getX(), 3);
+    EXPECT_EQ(p.getY(), 4);
 }
 
-TEST(PointTest, ParameterizedConstructor) {
-    Point<double> p(3.5, 4.2);
-    EXPECT_DOUBLE_EQ(p.getX(), 3.5);
-    EXPECT_DOUBLE_EQ(p.getY(), 4.2);
+TEST(PointTest, DoublePrecision) {
+    Point<double> p(3.14, 2.71);
+    EXPECT_DOUBLE_EQ(p.getX(), 3.14);
+    EXPECT_DOUBLE_EQ(p.getY(), 2.71);
 }
 
-TEST(TrapezoidTest, Area) {
+TEST(TrapezoidTest, AreaCalculation) {
     Trapezoid<int> t(0, 0, 4, 0, 1, 3, 3, 3);
-    EXPECT_NEAR(t.area(), 10.0, 1e-6);
+    EXPECT_NEAR(t.area(), 9.0, 0.001);
 }
 
-TEST(TrapezoidTest, Center) {
+TEST(TrapezoidTest, CenterCalculation) {
     Trapezoid<int> t(0, 0, 4, 0, 1, 3, 3, 3);
-    Point<int> c = t.center();
-    EXPECT_EQ(c.getX(), 2);
-    EXPECT_EQ(c.getY(), 1);
+    auto center = t.center();
+    EXPECT_EQ(center.getX(), 2);
+    EXPECT_EQ(center.getY(), 1);
 }
 
-TEST(RhombusTest, Area) {
-    Rhombus<int> r(0, 0, 3, 4, 8, 0, 5, -4);
-    EXPECT_NEAR(r.area(), 24.0, 1e-6);
+
+TEST(RhombusTest, AreaCalculation) {
+    Rhombus<int> r(0, 0, 2, 2, 4, 0, 2, -2);
+    EXPECT_NEAR(r.area(), 8.0, 0.001);
 }
 
-TEST(PentagonTest, Area) {
-    Pentagon<int> p(0, 0, 1, 2, 3, 3, 4, 1, 2, -1);
-    EXPECT_NEAR(p.area(), 8.5, 1e-6);
+
+TEST(PentagonTest, AreaCalculation) {
+    Pentagon<int> p(0, 0, 2, 1, 3, 4, 1, 5, -1, 3);
+    EXPECT_NEAR(p.area(), 12.0, 0.001);
 }
 
-TEST(ArrayTest, PushBackAndAccess) {
+TEST(ArrayTest, BasicOperations) {
     Array<std::shared_ptr<Figure<int>>> arr;
-    arr.push_back(std::make_shared<Trapezoid<int>>(0, 0, 2, 0, 1, 1, 1, 1));
+    
+    auto fig1 = std::make_shared<Trapezoid<int>>(0, 0, 4, 0, 1, 3, 3, 3);
+    arr.push_back(fig1);
     EXPECT_EQ(arr.size(), 1);
-    EXPECT_NEAR((*arr[0]).area(), 1.0, 1e-6);
-}
-
-TEST(ArrayTest, Erase) {
-    Array<std::shared_ptr<Figure<int>>> arr;
-    arr.push_back(std::make_shared<Trapezoid<int>>(0,0,2,0,1,1,1,1));
-    arr.push_back(std::make_shared<Rhombus<int>>(0,0,1,1,2,0,1,-1));
+    
+    auto fig2 = std::make_shared<Rhombus<int>>(0, 0, 2, 2, 4, 0, 2, -2);
+    arr.push_back(fig2);
+    EXPECT_EQ(arr.size(), 2);
+    
     arr.erase(0);
     EXPECT_EQ(arr.size(), 1);
-    EXPECT_NEAR((*arr[0]).area(), 1.0, 1e-6);
 }
 
 TEST(ArrayTest, TotalArea) {
     Array<std::shared_ptr<Figure<int>>> arr;
-    arr.push_back(std::make_shared<Trapezoid<int>>(0,0,2,0,1,1,1,1));
-    arr.push_back(std::make_shared<Rhombus<int>>(0,0,1,1,2,0,1,-1));
-    EXPECT_NEAR(arr.total_area(), 2.0, 1e-6);
+    
+    auto fig1 = std::make_shared<Trapezoid<int>>(0, 0, 4, 0, 1, 3, 3, 3);
+    auto fig2 = std::make_shared<Rhombus<int>>(0, 0, 2, 2, 4, 0, 2, -2);
+    
+    arr.push_back(fig1);
+    arr.push_back(fig2);
+    
+    EXPECT_NEAR(arr.total_area(), 9.0 + 8.0, 0.001);
 }
 
 int main(int argc, char **argv) {
